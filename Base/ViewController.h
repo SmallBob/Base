46c20e2f5f9f260c31cef5fac585aaeb1d7e6c8d
王凯 <wangkai@wangkaidemacbook-pro.local>
2016-03-11 11:44:57 +0800

    Initial Commit

---
+++ Base/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Base
+//
+//  Created by 王凯 on 16/3/11.
+//  Copyright © 2016年 kai wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Base/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Base
+//
+//  Created by 王凯 on 16/3/11.
+//  Copyright © 2016年 kai wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
